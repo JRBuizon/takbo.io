b4f55451c306da57b1606b496f8e412f92f8d53d
Jeremiah Buizon <buizonjeremiah@gmail.com>
2022-04-02 21:24:33 +0800

    logos and buttons

--- src/DeathScreen.gd
+++ src/DeathScreen.gd
@@ -5,3 +5,15 @@ onready var animation = $AnimationPlayer
 func fadeIn():
 	animation.play("popIn")
 
+
+
+func _on_SHARE_button_down():
+	get_tree().change_scene("res://src/ShareScreen.tscn")
+
+
+func _on_RETRY_button_down():
+	get_tree().change_scene("res://src/World.tscn")
+
+
+func _on_MENU_button_down():
+	get_tree().change_scene("res://src/MainMenu.tscn")
--- src/World.gd
+++ src/World.gd
@@ -12,8 +12,9 @@ var startedGame = false
 var pastYlevel = 488
 var ylevel = 0
 
-const totalVariations = 7
-const absoluteVariations = 3
+const totalVariations = 6
+const absoluteVariationsD = 3
+const absoluteVariationsU = 2
 const snapHeight = 16
 const highestPossible = 424
 const lowestPossible = 536
@@ -33,20 +34,20 @@ func _physics_process(delta):
 
 func rand_ylevel():
 	#Randomizes Y Level based on a randi from -3 to 3 multiplied by snapHeight
-	ylevel = pastYlevel-((randi() % totalVariations - absoluteVariations) * snapHeight)
+	ylevel = pastYlevel-((randi() % totalVariations - absoluteVariationsD) * snapHeight)
 	
 	if pastYlevel == ylevel: #If current Y Level is the same as the last Y Level
 		var choice = randi() % 2 #Randomly pick 1 or 0
 		if choice > 0: #If 0 go up and shuffle
-			ylevel = pastYlevel-((randi() % absoluteVariations + 1) * snapHeight)
+			ylevel = pastYlevel-((randi() % absoluteVariationsU + 1) * snapHeight)
 		else: #If not go down and shuffle
-			ylevel = pastYlevel+((randi() % absoluteVariations + 1) * snapHeight)
+			ylevel = pastYlevel+((randi() % absoluteVariationsD + 1) * snapHeight)
 		
 	if ylevel < highestPossible: #If above highest Possible Y level go down and shuffle
-		ylevel = pastYlevel+((randi()%absoluteVariations + 1)*snapHeight)
+		ylevel = pastYlevel+((randi()%absoluteVariationsD + 1)*snapHeight)
 	
 	if ylevel > lowestPossible: #If below lowest Possible Y level go up and shuffle
-		ylevel = pastYlevel-((randi()%absoluteVariations + 1)*snapHeight)
+		ylevel = pastYlevel-((randi()%absoluteVariationsU + 1)*snapHeight)
 	
 	pastYlevel = ylevel #setting the current Y Level as the past Y Level for the next platform
 	return ylevel #For putting in the Vector2 later
