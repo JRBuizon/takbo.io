754130f0c6d7bd2da3b101368ec7b6cc587febce
ramoncatane <ramoncatane@gmail.com>
2022-04-13 00:47:53 +0800

    refactor: Trying experimental keyboard and some config changes

--- src/Scripts/ShareScreen.gd
+++ src/Scripts/ShareScreen.gd
@@ -59,11 +59,11 @@ func _on_Timer_timeout():
 	get_tree().change_scene("res://src/Scenes/MainMenu.tscn")
 
 
-func _on_LineEdit_focus_entered():
-	# Prompts the user to input their username
-	var input_value = JavaScript.eval("window.prompt('Enter name')")
-	if input_value != null:
-		nameInput.text = input_value
-	# Only needs to release focus if game is running on a mobile device	
-	if OS.has_touchscreen_ui_hint():
-		nameInput.release_focus()
+#func _on_LineEdit_focus_entered():
+#	# Prompts the user to input their username
+#	var input_value = JavaScript.eval("window.prompt('Enter name')")
+#	if input_value != null:
+#		nameInput.text = input_value
+#	# Only needs to release focus if game is running on a mobile device	
+#	if OS.has_touchscreen_ui_hint():
+#		nameInput.release_focus()
