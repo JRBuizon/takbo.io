70ea30613346138dd2a253b32a73956901dd31c7
ramoncatane <ramoncatane@gmail.com>
2022-04-14 20:25:15 +0800

    refactor: Buttons don't take focus anymore

--- src/Scripts/VirtualKeyboard.gd
+++ src/Scripts/VirtualKeyboard.gd
@@ -1,5 +1,14 @@
 extends Control
 
+func on_key_pressed(key: String):
+	var target = get_focus_owner()
+	if target is TextEdit or target is LineEdit:
+		target.insert_text_at_cursor(key)
+
 # Closes the keyboard
 func _on_Enter_pressed():
 	self.hide()
+
+
+func _on_TextureButton_pressed():
+	on_key_pressed("q")
