e12b368ec131dcb5a664a615d6eea3709d224029
Jeremiah Buizon <buizonjeremiah@gmail.com>
2022-05-02 21:35:21 +0800

    deleted some stuff

--- PointsUp.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _ready():
-	yield(get_tree().create_timer(3), "timeout")
-	$Tween.interpolate_property($Label, "position:y", 0.0, -20.0, 1.0, 10, Tween.EASE_OUT)
-	$Tween.interpolate_property($Label, "modulate:a", 0.0, 255.0, 1.0, 10, Tween.EASE_OUT)
-	
