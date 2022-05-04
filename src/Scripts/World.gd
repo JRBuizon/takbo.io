51f1708d7a1650db67a762ac3e345bc1b90263b5
ramoncatane <ramoncatane@gmail.com>
2022-05-04 20:53:45 +0800

    refactor: banger to end all bangers

--- src/Scripts/World.gd
+++ src/Scripts/World.gd
@@ -71,6 +71,8 @@ func _ready():
 	#jump and death sfx
 	audio = AudioStreamPlayer.new()
 	audio.volume_db = -10
+	if Global.Character == Global.Gab:
+		Global.music_player.volume_db = 5
 	add_child(audio)
 	audio.pause_mode = Node.PAUSE_MODE_PROCESS
 	
